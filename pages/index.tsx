1e4ce6d07e4e4b1d4333a3977d18127b88313874
edy <edy@apyhub.com>
2023-01-06 11:05:57 +0700

    x

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,42 @@ import {
   Card,
   Container,
   Group,
+  Modal,
   Stack,
   Text,
+  TextInput,
   Title,
 } from "@mantine/core";
 
 import { IconFlame } from "@tabler/icons";
+import { useState } from "react";
 
-const Habit = ({ title }: { title: string }) => {
+import { useForm } from "@mantine/form";
+
+const habits = [
+  {
+    id: "1",
+    title: "Gym",
+    dateAdded: "2021-01-01",
+    datesCompleted: ["2021-01-01", "2021-01-02", "2021-01-03"],
+  },
+  {
+    id: "2",
+    title: "Meditate",
+  },
+];
+
+const Habit = ({
+  habit,
+}: {
+  habit: {
+    title: string;
+  };
+}) => {
   return (
     <Card withBorder>
       <Group position="apart">
-        <Text>{title}</Text>
+        <Text>{habit.title}</Text>
         <Group>
           <Group spacing={1}>
             <IconFlame />
@@ -27,16 +51,53 @@ const Habit = ({ title }: { title: string }) => {
   );
 };
 
+const CreateHabitButton = () => {
+  const [opened, setOpened] = useState(false);
+  const form = useForm({
+    initialValues: {
+      title: "",
+    },
+  });
+
+  const handleSubmit = (values: any) => {
+    // add habit to habits array
+    habits.concat({
+      id: Math.random().toString(),
+      title: values.title,
+      dateAdded: new Date().toISOString(),
+      datesCompleted: [],
+    });
+    setOpened(false);
+  };
+  return (
+    <>
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title="Create New Habit"
+      >
+        <form onSubmit={form.onSubmit(handleSubmit)}>
+          <Stack>
+            <TextInput {...form.getInputProps("title")} />
+            <Button type="submit">Add</Button>
+          </Stack>
+        </form>
+      </Modal>
+      <Button onClick={() => setOpened(true)}>Create Habit</Button>
+    </>
+  );
+};
+
 export default function Home() {
   return (
     <>
       <Container>
         <Title>Habits</Title>
-        <Button>Create Habit</Button>
+        <CreateHabitButton />
         <Stack>
-          <Habit title="Gym" />
-          <Habit title="Meditate" />
-          <Habit title="Read" />
+          {habits.map((habit) => (
+            <Habit habit={habit} key={habit.id} />
+          ))}
         </Stack>
       </Container>
     </>
